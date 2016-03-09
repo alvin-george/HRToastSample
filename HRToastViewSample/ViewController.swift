e6c50642760be8500c8d884d7410f44b2808d116
Alvin George <alvingeorge@fingents-mini-5.ftsindia.in>
2016-03-09 12:16:31 +0530

    normal change

--- HRToastViewSample/ViewController.swift
+++ HRToastViewSample/ViewController.swift
@@ -12,7 +12,33 @@ class ViewController: UIViewController {
 
  override func viewDidLoad() {
   super.viewDidLoad()
-  // Do any additional setup after loading the view, typically from a nib.
+
+ }
+ override func viewDidAppear(animated: Bool) {
+
+  //Usage:
+  self.view.makeToast(message: "Simple Toast")
+  self.view.makeToast(message: "Simple Toast", duration: 2.0, position:HRToastPositionTop)
+
+  self.view.makeToast(message: "Simple Toast", duration: 2.0, position: HRToastPositionCenter, image: UIImage(named: "ic_120x120")!)
+
+  self.view.makeToast(message: "It is just awesome", duration: 2.0, position: HRToastPositionDefault, title: "Simple Toast")
+
+  self.view.makeToast(message: "It is just awesome", duration: 2.0, position: HRToastPositionCenter, title: "Simple Toast", image: UIImage(named: "ic_120x120")!)
+
+
+  //Toast Activity
+  self.view.makeToastActivity()
+  self.view.makeToastActivity(position: HRToastPositionCenter)
+  self.view.makeToastActivity(position: HRToastPositionDefault, message: "Loading")
+  self.view.makeToastActivityWithMessage(message: "Loading")
+
+
+  // Hide Toast
+  self.view.hideToast(toast: self.view)
+  self.view.hideToast(toast: self.view, force: true)
+  self.view.hideToastActivity()
+
  }
 
  override func didReceiveMemoryWarning() {
